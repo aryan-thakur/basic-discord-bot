1d6418f8e6b2197e8f660d3648e1c6565dff59e0
Aryan Thakur <aryanthakur@Aryan.local>
2021-10-10 23:10:08 +0530

    initial commit

---
+++ command.js
@@ -0,0 +1,18 @@
+const { prefix } = require('./config.json');
+
+module.exports = (client, aliases, callback) => {
+  if(typeof aliases === 'string'){
+    aliases = [aliases];
+  }
+  client.on('messageCreate', (message) => {
+    const { content } = message;
+    
+    aliases.forEach(a => {
+      const command = `${prefix}${a}`;
+      if(content.startsWith(`${command} `) || content === `${command}`){
+        callback(message);
+        console.log('Command '+content+" detected!");
+      }
+      })
+  })
+};
